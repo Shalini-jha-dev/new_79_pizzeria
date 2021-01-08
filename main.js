c0133e31a3f9fd14a873d58d2f5ce5762a3f0a74
Shalini-jha-dev <74014550+Shalini-jha-dev@users.noreply.github.com>
2021-01-08 19:15:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,16 @@
-menu_list_array = ["Chicken Tandoori Pizza",
-                   "Veg Supreme Pizza",
-                   "Paneer Tikka Pizza",
-                   "Deluxe Veggie Pizza",
-                   "Veg Extravaganza Pizza"];
-
-function get_menu(){
-    var htmldata
-    htmldata = "<ol class='menu_list'>"
-    menu_list_array.sort();
-    for(var i=0;i<menu_list_array.length;i++){
-        htmldata = htmldata+'<li>'+ menu_list_array[i] + '</li>'
+function getParagraph1(){
+    var inputs=[];
+    for(var i = 1; i <= 6; i++){
+    inputs.push(document.getElementById("para_1_input_" + i).value);
     }
-htmldata = htmldata+"</ol>"
-document.getElementById("display_menu").innerHTML = htmldata;
+    inputs.join(". ");
+    document.getElementById("showParagraph1").innerHTML=inputs.join(". ");
 }
-function add_item(){
-    var htmldata;
-    var item=document.getElementById("add_item").value;
-    menu_list_array.push(item);
-    menu_list_array.sort();
-    htmldata="<section class='cards'>"
-    for(var i=0;i<menu_list_array.length;i++){
-
-        htmldata = htmldata + '<div class="cards">' + '<img src="images/pizzaImg.png"/>' + menu_list_array[i] + '</div>'
+function getParagraph2(){
+    var inputs=[];
+    for(var i = 1; i <= 6; i++){
+    inputs.push(document.getElementById("para_2_input_" + i).value);
     }
-    htmldata = htmldata+"</section>"
-    document.getElementById("display_addedmenu").innerHTML = htmldata;
+    inputs.join(". ");
+    document.getElementById("showParagraph2").innerHTML=inputs.join(". ");
 }
